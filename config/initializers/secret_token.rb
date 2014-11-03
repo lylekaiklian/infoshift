5ea4a09fa324c0efa0e92498a9648dd4340727a4
Lyle <lyle.kaiklian@infoshiftinc.com>
2014-11-03 17:51:53 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Infoshift::Application.config.secret_key_base = 'c964f4435ee776666d81116a9e433cb1c7f66a035eaf9f3bcebd4b0f6b2f5b7d86dd7b6b43a9e0c82be6b25508f52f833bb427f2ecdfda45372b7d5bcb9c2bc7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Infoshift::Application.config.session_store :cookie_store, key: '_infoshift_session'
